c3daec5525d3ec51dd38306a069b5f247599eaec
Laurence Hartgill <laurence@hartgill.co.uk>
2024-04-26 21:43:49 +0100

    feat: Export Token and Constructor for use by consumers

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { default as createContainer } from './createContainer';
 export { default as Container } from './Container';
 export { Lifecycle } from './lifecycles/Lifecycle';
 export { default as injectable } from './decorators/injectable';
+
+export type { Token } from './types/Token';
+export type { Constructor } from './types/Constructor';
