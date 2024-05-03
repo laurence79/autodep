e6b11e804d14c2362afb781a6155f27f49a63f01
Laurence Hartgill <laurence@hartgill.co.uk>
2024-05-03 09:11:11 +0100

    fix: Registration options type too loose

--- src/types/RegistrationOptions.ts
+++ src/types/RegistrationOptions.ts
@@ -4,7 +4,7 @@ import type { Lifecycle } from '../lifecycles/Lifecycle';
  * Options affecting the registration and resolution of a type.
  */
 export type RegistrationOptions<TLifecycle = Lifecycle> = {
-  lifecycle: TLifecycle;
+  readonly lifecycle: TLifecycle;
 };
 
 export type SingletonRegistrationOptions = RegistrationOptions<
