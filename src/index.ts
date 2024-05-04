d83d875af01cd3f2fa12edb04f15d28e872ac5d5
Laurence Hartgill <laurence@hartgill.co.uk>
2024-05-04 09:14:41 +0100

    fix: Don't import reflect metadata - consumer should do that

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import 'reflect-metadata';
-
 export { default as createContainer } from './createContainer';
 export { default as Container } from './Container';
 export { Lifecycle } from './lifecycles/Lifecycle';
